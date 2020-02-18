b22d5b19c5393ac563535d0216d82f682eb3a3e1
Benoit Florimond <benoitflorimond50@gmail.com>
2020-02-18 22:55:38 +0100

    add movement manager

--- main/applications/inc/movementManager.h
+++ main/applications/inc/movementManager.h
@@ -18,6 +18,13 @@
 
 /* ____________________________________________________________________________ */
 /* Enum 																		*/
+typedef enum {
+    MOVEMENT_STOP = 0,
+    MOVEMENT_FORWARD,
+    MOVEMENT_BACKWARD,
+    MOVEMENT_ROTATION_LEFT,
+    MOVEMENT_ROTATION_RIGHT,
+} movementType_e;
 
 /* ____________________________________________________________________________ */
 /* Struct																	 	*/
@@ -25,5 +32,9 @@
 /* ____________________________________________________________________________ */
 /* Public function prototypes 													*/
 void vMVT_Process(void* pvParameters);
+
+void vMVT_Move(movementType_e movement);
+
+void vMVT_EmergencyStop(void);
 
 #endif //__MOVEMENTMANAGER_H__
--- main/applications/src/movementManager.c
+++ main/applications/src/movementManager.c
@@ -18,25 +18,29 @@
 #define PULSE_WIDTH_MIN_MS (1.0F)
 #define PULSE_WIDTH_MAX_MS (2.0F)
 
+#define SPEED_FORWARD (50.0F)
+#define SPEED_BACKWARD (50.0F)
+#define SPEED_ROTATION (30.0F)
+#define SPEED_STOP (0.0F)
+
+#define FORWARD_DELAY_TICKS (pdMS_TO_TICKS(1000U))
+#define BACKWARD_DELAY_TICKS (pdMS_TO_TICKS(1000U))
+#define ROTATION_DELAY_TICKS (pdMS_TO_TICKS(1000U))
+
 /* ____________________________________________________________________________ */
 /* Enum  																		*/
-typedef enum {
-    STOP = 0,
-    FORWARD,
-    BACKWARD,
-    ROTATION_LEFT,
-    ROTATION_RIGHT,
-} movementType_e;
 
 /* ____________________________________________________________________________ */
 /* Struct																		*/
 
 /* ____________________________________________________________________________ */
 /* Static prototypes 															*/
+static void _endMovementTimerCallback(void* timer);
 
 /* ____________________________________________________________________________ */
 /* Static variables 															*/
-QueueHandle_t _queueForMovement = NULL;
+static QueueHandle_t _queueForMovement = NULL;
+static TimerHandle_t _timerForMovement = NULL;
 
 /* ____________________________________________________________________________ */
 /* ISR handlers 																*/
@@ -45,13 +49,72 @@ QueueHandle_t _queueForMovement = NULL;
 /* Public functions 															*/
 void vMVT_Process(void* pvParameters)
 {
+    movementType_e movementEvent = { 0 };
+
+    _queueForMovement = xQueueCreate(5U, sizeof(movementEvent));
+    _timerForMovement = xTimerCreate("Timer to end movement", FORWARD_DELAY_TICKS, pdFALSE, NULL, _endMovementTimerCallback);
+
     bSERVO_RegisterServo(SERVO_LEFT_GPIO_NUM, PULSE_WIDTH_MIN_MS, PULSE_WIDTH_MAX_MS);
     bSERVO_RegisterServo(SERVO_RIGHT_GPIO_NUM, PULSE_WIDTH_MIN_MS, PULSE_WIDTH_MAX_MS);
 
-    // _queueForMovement = xQueueCreatse();
-
     for (;;) {
+        xQueueReceive(_queueForMovement, &movementEvent, portMAX_DELAY);
+
+        switch (movementEvent) {
+        case MOVEMENT_STOP:
+            bSERVO_SetOrder(SERVO_LEFT_GPIO_NUM, SPEED_STOP, true);
+            bSERVO_SetOrder(SERVO_RIGHT_GPIO_NUM, SPEED_STOP, true);
+            xTimerStop(_timerForMovement, TIMER_API_DEFAULT_TIMEOUT);
+            break;
+        case MOVEMENT_FORWARD:
+            bSERVO_SetOrder(SERVO_LEFT_GPIO_NUM, SPEED_FORWARD, true);
+            bSERVO_SetOrder(SERVO_RIGHT_GPIO_NUM, SPEED_FORWARD, true);
+            xTimerChangePeriod(_timerForMovement, FORWARD_DELAY_TICKS, TIMER_API_DEFAULT_TIMEOUT);
+            xTimerReset(_timerForMovement, TIMER_API_DEFAULT_TIMEOUT);
+            break;
+        case MOVEMENT_BACKWARD:
+            bSERVO_SetOrder(SERVO_LEFT_GPIO_NUM, SPEED_BACKWARD, false);
+            bSERVO_SetOrder(SERVO_RIGHT_GPIO_NUM, SPEED_BACKWARD, false);
+            xTimerChangePeriod(_timerForMovement, BACKWARD_DELAY_TICKS, TIMER_API_DEFAULT_TIMEOUT);
+            xTimerReset(_timerForMovement, TIMER_API_DEFAULT_TIMEOUT);
+            break;
+        case MOVEMENT_ROTATION_LEFT:
+            bSERVO_SetOrder(SERVO_LEFT_GPIO_NUM, SPEED_ROTATION, false);
+            bSERVO_SetOrder(SERVO_RIGHT_GPIO_NUM, SPEED_ROTATION, true);
+            xTimerChangePeriod(_timerForMovement, ROTATION_DELAY_TICKS, TIMER_API_DEFAULT_TIMEOUT);
+            xTimerReset(_timerForMovement, TIMER_API_DEFAULT_TIMEOUT);
+            break;
+        case MOVEMENT_ROTATION_RIGHT:
+            bSERVO_SetOrder(SERVO_LEFT_GPIO_NUM, SPEED_ROTATION, true);
+            bSERVO_SetOrder(SERVO_RIGHT_GPIO_NUM, SPEED_ROTATION, false);
+            xTimerChangePeriod(_timerForMovement, ROTATION_DELAY_TICKS, TIMER_API_DEFAULT_TIMEOUT);
+            xTimerReset(_timerForMovement, TIMER_API_DEFAULT_TIMEOUT);
+            break;
+        default:
+            break;
+        }
     }
 }
+
+void vMVT_Move(movementType_e movement)
+{
+    xQueueSend(_queueForMovement, &movement, WRITE_IN_QUEUE_DEFAULT_TIMEOUT);
+}
+
+void vMVT_EmergencyStop(void)
+{
+    movementType_e movement = MOVEMENT_STOP;
+
+    xQueueReset(_queueForMovement);
+    xQueueSendToFront(_queueForMovement, &movement, 0U);
+}
+
 /* ____________________________________________________________________________ */
 /* Static functions 															*/
+
+static void _endMovementTimerCallback(void* timer)
+{
+    movementType_e movement = MOVEMENT_STOP;
+
+    xQueueSendToFront(_queueForMovement, &movement, 0U);
+}
